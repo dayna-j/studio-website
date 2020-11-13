184eadbdfeab3d28ec9a4c3a50e7b9615695ee15
Dayna <dayna@Daynas-MacBook-Pro.local>
2020-11-13 10:42:51 -0700

    .

--- index.php
+++ index.php
@@ -1,52 +1,104 @@
 <!doctype html>
 <html lang="en">
-  <head>
-      <title>Moonflower Productions</title>
+
+<head>
+    <title>Moonflower Productions</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="./styles/scss/dist/main.css">
 </head>
 
 </body>
 <div class="fluid-container">
     <header class="">
-
         <nav class="navbar navbar-expand-md navbar-dark">
-		<div class="container">
-            <!-- <div class="container-wrapper"> -->
-            <a href="#" class="navbar-brand">Moonflower Productions</a>
-            <button type="button" class="navbar-toggler collapsed" data-toggle="collapse" aria-expanded="false" data-target="#main-nav">
-            <span class="navbar-toggler-icon"></span>
-			</button>
-			
-			<div id="main-nav" class="collapse navbar-collapse">
-				<ul class="navbar-nav justify-content-lg-center">
-					<li><a href="#" class="nav-item nav-link active absolute-center">About</a></li>
-					<li><a href="#" class="nav-item nav-link absolute-center">Mixing</a></li>
-                    <li><a href="#" class="nav-item nav-link absolute-center">Mastering</a></li>
-                    <li><a href="#" class="nav-item nav-link absolute-center">Consultation</a></li>
-                    <li><a href="#" class="nav-item nav-link absolute-center">Contact</a></li>
-				</ul>
-			</div>
-        </div>
-    <!-- </div> -->
-        
-	    </nav>
+            <div class="container">
+                <a href="#" class="navbar-brand">Moonflower Productions</a>
+                <button type="button" class="navbar-toggler collapsed" data-toggle="collapse" aria-expanded="false"
+                    data-target="#main-nav">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div id="main-nav" class="collapse navbar-collapse">
+                    <ul class="navbar-nav justify-content-lg-center">
+                        <li><a href="#" class="px-3 nav-item nav-link active absolute-center">About</a></li>
+                        <li><a href="#" class="px-3 nav-item nav-link absolute-center">Mixing</a></li>
+                        <li><a href="#" class="px-3 nav-item nav-link absolute-center">Mastering</a></li>
+                        <li><a href="#" class="px-3 nav-item nav-link absolute-center">Consultation</a></li>
+                        <li><a href="#" class="px-3 nav-item nav-link absolute-center">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     </header>
 
-<section>
+    <section id='background-overlay' style='background-image:url("../../sequencer2.png")' class='w-100'>
 
-</section>
+    </section>
+
+    <section>
+        <div class="container">
+        <div class="row">
+
+            <div class="col">
+                
+                <div class="row">
+                    <h1 class='p-5 text-white text-responsive mx-auto' style=''>Full Scale Audio Production Services</h1>
+                </div>
+
+                <div class="row">
+                    <div class="col">
+                        <div class="row">
+                            <div class="col"></div>
+                            <div class="col"></div>
+                        </div>
+                        <div class="row">
+                            <div class="col"></div>
+                            <div class="col"></div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="row">
+                            <div class="col"></div>
+                            <div class="col"></div>
+                        </div>
+                        <div class="row">
+                            <div class="col"></div>
+                            <div class="col"></div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="row">
+                            <div class="col"></div>
+                            <div class="col"></div>
+                        </div>
+                        <div class="row">
+                            <div class="col"></div>
+                            <div class="col"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
 
 
 </div>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+</script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+</script>
 </body>
 
 
